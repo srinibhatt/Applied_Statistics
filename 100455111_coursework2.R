feb02031b9b6bc1bfbb971d0c742558b549e3031
srinibhatt <63896350+srinibhatt@users.noreply.github.com>
2023-12-19 21:10:13 +0000

    commit

--- 100455111_coursework2.R
+++ 100455111_coursework2.R
@@ -11,3 +11,22 @@ q3 <- quantile(vinegar_data$pH, 0.75)
 
 # Calculate IQR manually
 iqr_value <- q3 - q1
+
+# Assuming 'vinegar_data' is your dataframe with 'Acidity' and 'Factory_Location' columns
+
+# Calculate IQR for each factory location
+iqr_values <- tapply(vinegar_data$pH, vinegar_data$Site,IQR)
+View(iqr_values)
+
+# Perform one-way ANOVA
+anova_result <- aov(vinegar_data$pH ~ vinegar_data$Site, data = vinegar_data)
+
+# Summary of ANOVA results
+summary(anova_result)
+
+# Perform pairwise t-tests
+pairwise_result <- pairwise.t.test(vinegar_data$pH, vinegar_data$Site, p.adj = "none")
+pairwise_result
+
+
+
